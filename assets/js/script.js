770d38d6389f5941d7f4b064d98f0fb5c7791c7d
Jocelyn <115833657+jjocelynn@users.noreply.github.com>
2023-02-01 16:44:31 -0500

    added enter key functionality and save coordinates to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,41 @@
 const apiKey = "pk.eyJ1IjoiaXNodmFsIiwiYSI6ImNsZGtmZHQ2bzE3Zngzb2xsZWpld25qYXEifQ.h-MDSqkuEyiG5MPawRdT9w";
-let searchHistory = [];
-let coordinatesHistory = [];
+let searchHistory = []; //saves user search histroy(to be used for local storage)
+let coordinatesHistory = []; //saves searched location coordinates(to be used for local storage)
+let existingValue = "false"; //verifies if user has already searched this term
 
 // on page load, get items from local storage and display as buttons
 $(function () {
+    coordinatesHistory = JSON.parse(localStorage.getItem("coordinates")) || [];
     searchHistory = JSON.parse(localStorage.getItem("location")) || [];
     for (i = 0; i < searchHistory.length; i++) {
         createButton(searchHistory[i]);
     }
 })
 
-//when search button is clicked, add the input to searchHistory array and save to local storage
+//when search button is clicked or enter key is pressed in the searchInput, run the code
 $("#searchButton").click(function () {
     let location = $("#searchInput").val();
+    runCode(location);
+})
+$("#searchInput").on("keydown", function (event) {
+    if (event.key == "Enter") { //checks if the key pressed is the enter key.
+        let location = $("#searchInput").val();
+        runCode(location);
+    }
+});
+
+//add the input to searchHistory array and save to local storage
+let runCode = function (location) {
     if (searchHistory.includes(location)) {
-        //dont add anything to history
+        existingValue = "true"; //sets existingValue to true (term has been already been searched)
     } else {
+        existingValue = "false"; //sets existingValue to false (term has not been searched)
         searchHistory.unshift(location);
         localStorage.setItem("location", JSON.stringify(searchHistory));
         createButton(location);
     }
-    
     mapCall(location);
-})
+}
 
 // wiki api call 
 let wikiCall = function (latitude, longitude) {
@@ -43,7 +56,7 @@ let wikiCall = function (latitude, longitude) {
                     console.error(error);
                     $("#wikiArticle").html("<p>No results found</p>");
                 }); // display error message to page if search is not found.
-        })     
+        })
 }
 
 // Map API call
@@ -65,7 +78,7 @@ let mapCall = function (location) {
                 center: [longitude, latitude], // starting position [lng, lat]
                 zoom: 15, // starting zoom
             });
-            
+
             //Sizing the scale appropriately
             let keyName = Object.keys(data.features[0]); //getting an array of object key names
 
@@ -76,6 +89,20 @@ let mapCall = function (location) {
                 let eastLon = data.features[0].bbox[3];
                 map.fitBounds([[southLat, westLon], [northLat, eastLon]]); //setting map boundaries
             }
+
+            // Creating a marker per area
+            //saving latitude and longitude to coordinates object
+            let coordinates = {
+                lat: latitude,
+                long: longitude
+            }
+
+            //if value does not already exist, add it to coordinatesHistory array and save to local storage
+            if (existingValue == "false") { 
+                coordinatesHistory.push(coordinates);
+                localStorage.setItem("coordinates", JSON.stringify(coordinatesHistory));
+            }
+
             wikiCall(latitude, longitude);
         })
         .catch(error => console.error(error));
@@ -98,5 +125,5 @@ $("#clearHistory").click(function () {
 //adding functionality to search history buttons
 $("#searchHistory").click(function (e) {
     let search = e.target.textContent;
-    mapCall(search);
+    runCode(search);
 });
