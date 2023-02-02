beeb0782bf71c0c0d57fc130538bd45e554586ab
Julian Ramirez <juliannrrra@outlook.es>
2023-02-02 14:40:48 -0500

    #fixxed

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,12 @@ const apiKey = "pk.eyJ1IjoiaXNodmFsIiwiYSI6ImNsZGtmZHQ2bzE3Zngzb2xsZWpld25qYXEif
 let searchHistory = []; //saves user search histroy(to be used for local storage)
 let coordinatesHistory = []; //saves searched location coordinates(to be used for local storage)
 let existingValue = "false"; //verifies if user has already searched this term
+let marker = []; //array of pinpoint markers
 
 //when you click on the WikiGO logo it takes you to the top
 document.querySelector("header").addEventListener("click", function () {
     window.scrollTo(0, 0);
 });
-
-//when you click on the search button or one of the searchHistory items it takes you to the wikiArticle
-
-
 
 function scrollToWikiArticle() {
     const wikiArticle = document.getElementById("wikiArticle");
@@ -19,28 +16,6 @@ function scrollToWikiArticle() {
         behavior: "smooth"
     });
 }
-
-
-//when you click on the WikiGO logo it takes you to the top
-document.querySelector("header").addEventListener("click", function () {
-    window.scrollTo(0, 0);
-});
-
-//when you click on the search button or one of the searchHistory items it takes you to the wikiArticle
-document.getElementById("searchButton").addEventListener("click", function () {
-    scrollToWikiArticle();
-});
-
-
-function scrollToWikiArticle() {
-    const wikiArticle = document.getElementById("wikiArticle");
-    window.scrollTo({
-        top: wikiArticle.offsetTop,
-        behavior: "smooth"
-    });
-}
-
-
 
 
 // on page load, get items from local storage and display as buttons
@@ -52,12 +27,13 @@ $(function () {
     }
 })
 
-//when search button is clicked or enter key is pressed in the searchInput, run the code
+//when search button is clicked or enter key is pressed, assign the value to "location", and run the code
 $("#searchButton").click(function () {
     let location = $("#searchInput").val();
     scrollToWikiArticle();
     runCode(location);
-})
+});
+
 $("#searchInput").on("keydown", function (event) {
     if (event.key == "Enter") { //checks if the key pressed is the enter key.
         let location = $("#searchInput").val();
@@ -66,13 +42,13 @@ $("#searchInput").on("keydown", function (event) {
     }
 });
 
-//add the input to searchHistory array and save to local storage
+// checks if the location is already in searchHistory array. if not, add it to searchHistory, local storage, and create button.
 let runCode = function (location) {
     if (searchHistory.includes(location)) {
-        existingValue = "true"; //sets existingValue to true (term has been already been searched)
+        existingValue = "true"; //sets existingValue to true (location has been already been searched)
     } else {
-        existingValue = "false"; //sets existingValue to false (term has not been searched)
-        searchHistory.unshift(location);
+        existingValue = "false"; //sets existingValue to false (location has not yet been searched)
+        searchHistory.push(location);
         localStorage.setItem("location", JSON.stringify(searchHistory));
         createButton(location);
     }
@@ -81,7 +57,7 @@ let runCode = function (location) {
 
 // wiki api call 
 let wikiCall = function (latitude, longitude) {
-    let apiUrl = `https://en.wikipedia.org/w/api.php?action=query&list=geosearch&gscoord=${latitude}%7C${longitude}&gsradius=5000&gslimit=1&format=json&origin=*`;//insert lat and lon from mapCall.
+    let apiUrl = `https://en.wikipedia.org/w/api.php?action=query&list=geosearch&gscoord=${latitude}%7C${longitude}&gsradius=5000&gslimit=10&format=json&origin=*`;//insert lat and lon from mapCall.
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
@@ -96,7 +72,6 @@ let wikiCall = function (latitude, longitude) {
                     // article overflow container 
                     document.getElementById("wikiArticle").style.width = "75%";
                     document.getElementById("wikiArticle").style.height = "9em";
-                    document.getElementById("wikiArticle").style.border = "2px solid #0038ff";
                     document.getElementById("wikiArticle").style.overflowY = "scroll";
                     document.getElementById("wikiArticle").style.overflowX = "hidden";
                 })
@@ -151,7 +126,15 @@ let mapCall = function (location) {
                 localStorage.setItem("coordinates", JSON.stringify(coordinatesHistory));
             }
 
-            wikiCall(latitude, longitude);
+            //creating pinpoints according to the lat and long stored in coordinatesHistory
+            for (let i = 0; i < coordinatesHistory.length; i++) {
+                pinPoint = new mapboxgl.Marker({})
+                    .setLngLat([coordinatesHistory[i].long, coordinatesHistory[i].lat])
+                    .addTo(map);
+                marker.push(pinPoint); //adding the pinpoint to marker array
+            }
+
+            wikiCall(latitude, longitude); //calling the wiki article
         })
         .catch(error => console.error(error));
 }
@@ -163,15 +146,24 @@ let createButton = function (locationBtn) {
     $("#searchHistory").append(button);
 }
 
-//clear search history button
+//clear history button (clears search history, coordinates, and markers)
 $("#clearHistory").click(function () {
     searchHistory = [];
     localStorage.setItem("location", JSON.stringify(searchHistory));
     $("#searchHistory").text("");
+
+    coordinatesHistory = [];
+    localStorage.setItem("coordinates", JSON.stringify(coordinatesHistory));
+
+    for (let i = 0; i < marker.length; i++) {
+        marker[i].remove();
+    }
+    marker = [];
 })
 
 //adding functionality to search history buttons
 $("#searchHistory").click(function (e) {
     let search = e.target.textContent;
+    scrollToWikiArticle();
     runCode(search);
 });
